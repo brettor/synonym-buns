7ca48c2efafa41b31729b23ce3d064a1f0af687a
Brett Nielsen <brettnielsen@rogers.com>
2017-09-06 10:34:33 -0400

    Expanded initial code for React boilerplate

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,1 +1,12 @@
-console.log('hi!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+	render(){
+		return(
+			<div>Hello!</div>
+		)
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
